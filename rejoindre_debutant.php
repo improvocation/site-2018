06eb6eddb47e6379d836be09c50118a068e6573e
GjeloshajAntoine <32301511+GjeloshajAntoine@users.noreply.github.com>
2018-05-01 17:29:17 +0200

    updload 1

---
+++ rejoindre_debutant.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="landing_flex.css">
+  </head>
+  <body>
+    <div class="fond">
+      <h1 class="landing-titre">Improvocation / Rejoindre L'impro</h1>
+      <div class="form"><!-- center the form --> 
+
+      </div>
+      <div class="bottom-container">
+          <div class="bottom-left">
+            <div class="inner-hop">
+              De nombreux matchs et spectacles, ainsi qu’un stage d’initiation à l’impro, sont organisés tout au long de l’année académique.
+            </div>
+          </div>
+          <div class="bottom-right">
+            <div class="inner-hop">
+              Et Hop !
+            </div>
+          </div>
+      </div>
+  </body>
+</html>
