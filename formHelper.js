eb9eff9a26653abbadc32e18d2e242352bd84d19
Antoine <gjeloshaj.antoine@gmail.com>
2018-08-26 16:43:09 +0200

    form validation and sending

--- formHelper.js
+++ formHelper.js
@@ -2,20 +2,31 @@
 function notEmpty(inputId,errorId){
   if (document.getElementById(inputId).value.length === 0) {
     document.getElementById(errorId).style.visibility='visible'
-    return true;
+    return false;
   } else {
     document.getElementById(errorId).style.visibility='hidden'
-    return false;
+    return true;
   }
 }
 function isEmail(inputId,errorId){
-  document.getElementById(errorId).style.visibility='visible'
-
-  return false;
+  let email = document.getElementById(inputId).value
+  if (validateEmail(email)) {
+    document.getElementById(errorId).style.visibility='hidden'
+    return true;
+  } else {
+    document.getElementById(errorId).style.visibility='visible'
+    return false;
+  }
 }
-
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 function toggleVisibility(){
 
+}
+function allFieldOk(allBools) {
+  return allBools.reduce((pre,elem)=>pre && elem,true)
 }
 function ajaxForm(formTag,url,callback) {
   let datas= new FormData(document.getElementById(formTag));
